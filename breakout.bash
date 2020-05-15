8b5c4f48ff8a73a72a59c38c061bc27eb12b2e05
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2020-05-14 22:44:00 -0300

    Delete file

--- breakout.bash
+++
@@ -1,2 +0,0 @@
-docker run --gpus all -it --rm -v $PWD:/tmp -w /tmp my-tensorflow-gpu-py3 python ./breakout_deep_q_learning.py --gpu --profile
-# docker run --gpus all -it --rm -v $(realpath ~/GitHub):/tf/notebooks -p 8888:8888 tensorflow/tensorflow:latest-gpu-py3-jupyter
